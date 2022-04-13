c0cb6410bef850a77567949c8834e999d01ef50c
ssc84 <ssc84@drexel.edu>
2022-04-13 13:17:45 -0400

    login with admin routing done

---
+++ src/Scanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Scanner() {
+  return (
+    <div>
+      <p>You are in the Scanning page!</p>
+    </div>
+  );
+}
+
+export default Scanner;
--- src/login.js
+++ src/login.js
@@ -1,35 +1,69 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const adminUsername = "admin";
+  const adminPassword = "admin123";
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  const handleRouting = () => {
+    return username === adminUsername && password === adminPassword;
+  };
+
   return (
-    <Box
-      sx={{
-        backgroundColor: "#EAEDED",
-        borderRadius: "5%",
-        position: "relative",
-        top: "100px",
-        height: "50vh",
-        display: "flex",
-        flexDirection: "column",
-        fontSize: "250%",
-        fontWeight: "500",
-        textAlign: "center",
-      }}
-    >
-      {"Login"}
-      <div style={{ position: "relative", top: "12%", height: "30%" }}>
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
-      </div>
-      <div style={{ position: "relative", top: "13%", height: "30%" }}>
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-        />
-      </div>
-    </Box>
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#EAEDED",
+          borderRadius: "5%",
+          position: "relative",
+          top: "100px",
+          height: "50vh",
+          display: "flex",
+          flexDirection: "column",
+          fontSize: "250%",
+          fontWeight: "500",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ position: "relative", top: "5%" }}>{"Welcome!"}</div>
+        <div style={{ position: "relative", top: "12%", height: "30%" }}>
+          <TextField
+            id="Username"
+            label="Username"
+            variant="outlined"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div style={{ position: "relative", top: "13%", height: "30%" }}>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div style={{ position: "relative", top: "5%" }}>
+          {/* <Link to="/scanner">Scanner</Link> */}
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleRouting()
+                ? navigate(`/scanner`)
+                : alert("Wrong Username/Password Combination!");
+            }}
+          >
+            LOGIN
+          </Button>
+        </div>
+      </Box>
+    </>
   );
 }
 
